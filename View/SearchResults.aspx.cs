cdd0fc6ce50c7769db57b730c794e3e8f3cc571d
Mynghado <kaamec@gmail.com>
2018-05-31 22:04:16 +0100

    Update : add SearchResults

---
+++ View/SearchResults.aspx.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace Esondage2018.View
+{
+    public partial class SearchResults : System.Web.UI.Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+            if (PreviousPage != null)
+            {
+                TextBox txtSearch = (TextBox)PreviousPage.FindControl("TextBox4");
+                lblSearch.Text = txtSearch.Text;
+            }
+
+            SqlConnection conn = new SqlConnection(@"Data Source=(LocalDB)\v11.0;AttachDbFilename=" + Server.MapPath("~\\App_Data\\mydatabase.mdf") + ";Integrated Security=True");
+            GridView1.DataSourceID = "";
+            
+            String query = "select * from utilisateur where (nom like '%' + @search + '%')";
+            SqlCommand sr = new SqlCommand(query, conn);
+            sr.Parameters.Add("@search", SqlDbType.NVarChar).Value = lblSearch.Text;
+
+            conn.Open();
+            sr.ExecuteNonQuery();
+            SqlDataAdapter da = new SqlDataAdapter();
+            da.SelectCommand = sr;
+            DataSet ds = new DataSet();
+            da.Fill(ds, "nom");
+            GridView1.DataSource = ds;
+            GridView1.DataBind();
+
+            query = "select * from poll where (question like '%' + @search + '%')";
+            SqlCommand sr2 = new SqlCommand(query, conn);
+            sr2.Parameters.Add("@search", SqlDbType.NVarChar).Value = lblSearch.Text;
+
+            sr2.ExecuteNonQuery();
+            SqlDataAdapter da2 = new SqlDataAdapter();
+            da2.SelectCommand = sr2;
+            DataSet ds2 = new DataSet();
+            da2.Fill(ds2, "nom");
+            GridView2.DataSource = ds2;
+            GridView2.DataBind();
+
+
+            conn.Close();
+        }
+
+        protected void LinkButton2_Click(object sender, EventArgs e)
+        {
+            Session["nom"] = "";
+            Response.Redirect("login.aspx");
+        }
+    }
+}
